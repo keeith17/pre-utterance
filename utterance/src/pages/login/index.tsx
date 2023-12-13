244af9b4f566d0edde1d2b929d25045ed16b6d0b
PARANOIAQUE\zuxic <paranoiaque17@gmail.com>
2023-12-14 00:45:48 +0900

    동영상 위치 값 수정

--- utterance/src/pages/login/index.tsx
+++ utterance/src/pages/login/index.tsx
@@ -4,10 +4,7 @@ export default function LoginPage() {
     return (
         <LoginWrap>
             <video width="100%" muted autoPlay loop>
-                <source
-                    src="../../../public/videos/Untitled.mp4"
-                    type="video/mp4"
-                />
+                <source src="/public/videos/Untitled.mp4" type="video/mp4" />
             </video>
             <div className="login">
                 <div className="logo">프로젝트 PANDORA</div>
