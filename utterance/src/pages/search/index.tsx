05fe4430dfd6c1014e6413b8432709fa10848135
PARANOIAQUE\zuxic <paranoiaque17@gmail.com>
2023-12-14 00:43:20 +0900

    링크 임시

--- utterance/src/pages/search/index.tsx
+++ utterance/src/pages/search/index.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function SearchPage() {
-    return <div>검색 페이지</div>;
+    return (
+        <div>
+            검색 페이지
+            <Link to="/LoginPage">로그인페이지가기</Link>
+        </div>
+    );
 }
