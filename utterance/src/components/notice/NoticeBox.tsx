3dee2925ab99d29f7b694a3d79eb82fdb498c142
PARANOIAQUE\zuxic <paranoiaque17@gmail.com>
2024-02-12 17:56:05 +0900

    공지 일단 출력만

--- utterance/src/components/notice/NoticeBox.tsx
+++ utterance/src/components/notice/NoticeBox.tsx
@@ -1,9 +1,78 @@
+import {
+    collection,
+    getDocs,
+    limit,
+    orderBy,
+    query,
+    where,
+} from "firebase/firestore";
 import { NoticeStyle } from "./NoticeBoxStyle";
+import { db } from "@/firebaseApp";
+import { useQuery } from "react-query";
+
+export interface CommentProps {
+    id: string;
+    comment: string;
+    uid: string;
+    email: string;
+    createdAt: string;
+    profileUrl: string;
+    imgUrl: string;
+    nickname: string;
+}
+
+export interface PostProps {
+    id: string; // 포스트 아이디
+    content: string;
+    createdAt: string;
+    imgUrl?: string;
+    uid: string; // 유저 아이디
+    likes?: string[];
+    likeCount?: number;
+    comments?: CommentProps[];
+    profileUrl?: string;
+    nickname?: string;
+    tag: string;
+}
 
 export default function NoticeBox() {
+    //공지 데이터 받아오기
+    const fetchNoticeData = async () => {
+        try {
+            const postRef = collection(db, "posts");
+            const postQuery = query(
+                postRef,
+                where("tag", "==", "notice"),
+                orderBy("createdAt", "desc"),
+                limit(6)
+            );
+            const NoticePostsSnapshot = await getDocs(postQuery);
+            const data = NoticePostsSnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            })) as PostProps[];
+            return data;
+        } catch (error) {
+            console.error("Error fetching posts:", error);
+        }
+    };
+    const { data: noticePosts } = useQuery("fetchNoticeData", fetchNoticeData, {
+        staleTime: 20000,
+    });
     return (
         <NoticeStyle>
-            <div className="noticeBox"></div>
+            <div className="noticeBox">
+                {noticePosts &&
+                    (noticePosts.length > 0 ? (
+                        noticePosts?.map((post: PostProps, index: number) => (
+                            <div className="postBox" key={index}>
+                                {post.content}
+                            </div>
+                        ))
+                    ) : (
+                        <div className="text">해당하는 게시 글이 없습니다</div>
+                    ))}
+            </div>
         </NoticeStyle>
     );
 }
--- utterance/src/components/notice/NoticeBoxStyle.ts
+++ utterance/src/components/notice/NoticeBoxStyle.ts
@@ -6,8 +6,14 @@ export const NoticeStyle = styled.div`
     .noticeBox {
         width: 100%;
         height: 100%;
+        padding: 7% 2% 2% 2%;
         background: rgba(255, 255, 255, 0.2);
         background: url("images/twitter/twitterframe.webp") no-repeat 50% 50%;
         background-size: 100% 100%;
+        .postBox {
+            width: 100%;
+            height: 16.66666%;
+            border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+        }
     }
 `;
